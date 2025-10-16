1f38abef497c9be64308dcbf9fff29f022be4f39
Jorge Rodriguez <jrodriguez@novosit.com>
2025-10-16 12:19:29 -0400

    minor fixes over modal

--- frontend/src/app/shared/modal/modal.component.ts
+++ frontend/src/app/shared/modal/modal.component.ts
@@ -1,7 +1,15 @@
-import { Component, EventEmitter, Input, Output, input } from '@angular/core';
+import { Component, EventEmitter, Output, input } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 
+/**
+ * A reusable component for displaying content in a modal window.
+ *
+ * @example
+ * <app-modal [isOpen]="isModalOpen" (modalClose)="isModalOpen = false">
+ *   <p>Modal content</p>
+ * </app-modal>
+ */
 @Component({
   selector: 'app-modal',
   standalone: true,
@@ -10,15 +18,24 @@ import { CommonModule } from '@angular/common';
   styleUrl: './modal.component.css',
 })
 export class ModalComponent {
-  @Input() isOpen = false;
+  /** Controls whether the modal is open or closed. */
+  isOpen = input<boolean>(false);
+
+  /** Event emitted when the modal is closed. */
   @Output() modalClose = new EventEmitter<void>();
+
+  /** Defines the size of the modal. */
   size = input<'basic' | 'medium' | 'big' | 'custom'>('basic');
+
+  /** Defines the custom width of the modal when `size` is 'custom'. */
   customWidth = input<string>('80%');
 
+  /** Emits the `modalClose` event to close the modal. */
   onClose() {
     this.modalClose.emit();
   }
 
+  /** @internal */
   get modalWidthClasses(): string | string[] {
     switch (this.size()) {
       case 'medium':
@@ -26,12 +43,13 @@ export class ModalComponent {
       case 'big':
         return 'max-w-7xl w-[80%]';
       case 'custom':
-        return ''; // clases se manejan inline con [ngStyle]
+        return ''; // classes are handled inline with [ngStyle]
       default:
         return 'max-w-md';
     }
   }
 
+  /** @internal */
   get modalStyle(): Record<string, string> | null {
     if (this.size() === 'custom') {
       return { width: this.customWidth() };
